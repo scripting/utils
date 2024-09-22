cf907ba60330a3c7d8cfb8137e823bac13473985
Dave Winer <dave.winer@gmail.com>
2024-09-22 10:59:39 -0400

    .

--- daveutils.js
+++ daveutils.js
@@ -1,7 +1,7 @@
-var myProductName = "daveutils", myVersion = "0.4.69";  
+var myProductName = "daveutils", myVersion = "0.4.70";  
 
 /*  The MIT License (MIT)
-	Copyright (c) 2014-2023 Dave Winer
+	Copyright (c) 2014-2024 Dave Winer
 	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
